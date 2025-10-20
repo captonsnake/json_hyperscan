c0ebb7aae31c97bb498c8efb0058ce3a259842c7
Samual McCracken <mccracken.landon@gmail.com>
2025-10-19 20:07:40 -0400

    add add patterns

--- src/json_hyperscan/json_hyperscan.py
+++ src/json_hyperscan/json_hyperscan.py
@@ -1,15 +1,21 @@
 from typing import Any
 
 
+class State:
+    pass
+
+
 class JSONHyperscan:
     def __init__(self, patterns: list[str] | None = None) -> None:
-        self.patterns = patterns or []
+        self.patterns: set[str] = set(patterns) if patterns else set()
+        self.__database: State | None = None
 
     def compile(self) -> None:
         pass
 
     def add_pattern(self, pattern: str) -> None:
-        pass
+        if pattern not in self.patterns:
+            self.patterns.add(pattern)
 
     def match_any(self, haystack: list | dict) -> bool:
         return True
