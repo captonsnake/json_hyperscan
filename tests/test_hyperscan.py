6916c306f5ced32f53e71aed2b6699d628d882e4
Samual McCracken <mccracken.landon@gmail.com>
2025-10-22 20:59:44 -0400

    add parameter names for clarity

--- tests/test_hyperscan.py
+++ tests/test_hyperscan.py
@@ -19,23 +19,20 @@ class TestHyperscan:
     @pytest.mark.parametrize(
             "json_path_pattern",
             [
-                "$.store.book[*].author", # The authors of all books
-                "$..author", # All authors
-                "$.store.*", # All things, both books and bicycles
-                "$.store..price", # The price of everything
-                "$..book[2]", # The third book
-                "$..book[-2]", # The second to last book
-                # "$..book[0,1]", # The first two books
-                "$..book[:2]", # All books from index 0 (inclusive) until index 2 (exclusive)
-                "$..book[1:2]", # All books from index 1 (inclusive) until index 2 (exclusive)
-                "$..book[-2:]", # Last two books
-                "$..book[2:]", # All books from index 2 (inclusive) to last
-                "$..book[?(@.isbn)]", # All books with an ISBN number
-                "$.store.book[?(@.price < 10)]", # All books in store cheaper than 10
-                # "$..book[?(@.price <= $['expensive'])]", # All books in store that are not "expensive"
-                # "$..book[?(@.author =~ /.*REES/i)]", # All books matching regex (ignore case)
-                "$..*", # Give me every thing
-                # "$..book.length()", # The number of books
+                pytest.param("$.store.book[*].author", id="authors_of_all_books"), # The authors of all books
+                pytest.param("$..author", id="all_authors"), # All authors
+                pytest.param("$.store.*", id="all_things"), # All things, both books and bicycles
+                pytest.param("$.store..price", id="price_of_everything"), # The price of everything
+                pytest.param("$..book[2]", id="third_book"), # The third book
+                pytest.param("$..book[-2]", id="second_to_last_book"), # The second to last book
+                pytest.param("$..book[0,1]", id="first_two_books, comma sep", marks=pytest.mark.xfail()), # The first two books
+                pytest.param("$..book[:2]", id="first_two_books"), # All books from index 0 (inclusive) until index 2 (exclusive)
+                pytest.param("$..book[1:2]", id="second_book"), # All books from index 1 (inclusive) until index 2 (exclusive)
+                pytest.param("$..book[-2:]", id="last_two_books"), # Last two books
+                pytest.param("$..book[2:]", id="books_from_index_2"), # All books from index 2 (inclusive) to last
+                pytest.param("$..book[?(@.isbn)]", id="books_with_isbn"), # All books with an ISBN number
+                pytest.param("$.store.book[?(@.price < 10)]", id="cheap_books"), # All books in store cheaper than 10
+                pytest.param("$..*", id="give_me_everything"), # Give me every thing
             ]
         )
     # yapf: enable
